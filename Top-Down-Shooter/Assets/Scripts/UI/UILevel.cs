b271ed27f5e559eeb05a5c6b419d581ecb895091
Furkan YÜKSEL <furkan1928yuksel@gmail.com>
2022-12-06 12:51:42 +0300

    Add : New ui design added from asset store

---
+++ Top-Down-Shooter/Assets/Scripts/UI/UILevel.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UILevel : MonoBehaviour
+{
+    [SerializeField] Image _fillableBar;
+    [SerializeField] TextMeshProUGUI _levelText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
