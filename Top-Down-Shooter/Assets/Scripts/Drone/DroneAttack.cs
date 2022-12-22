2a6953c3d7a6842407965b54237ac3565671dc93
Furkan YÜKSEL <furkan1928yuksel@gmail.com>
2022-12-23 00:21:16 +0300

    Target belirleme

--- Top-Down-Shooter/Assets/Scripts/Drone/DroneAttack.cs
+++ Top-Down-Shooter/Assets/Scripts/Drone/DroneAttack.cs
@@ -6,18 +6,48 @@ public class DroneAttack : MonoBehaviour
 {
     Vector3 _enemyPos;
     [SerializeField] float lookAtSpeed=1;
-
-    void FixedUpdate()
+    [SerializeField] Projectile projectile;
+    [SerializeField] int _damage = 5;
+    [SerializeField] float _rate;
+    Shooter[] shooters;    
+    float _fireRateCounter = 0f;
+    bool _canFire = true;
+    private void Awake() 
     {
-        //transform.LookAt(_enemyPos*Time.deltaTime*lookAtSpeed);    
+        shooters = GetComponentsInChildren<Shooter>();
+        foreach (var shooter in shooters)
+        {
+            shooter.InitShooter(projectile, _damage, 10);
+        }
+    }
+    void Update()
+    {
+        //transform.LookAt(_enemyPos);   
+        Vector3 targetDir = _enemyPos - transform.position;
+        targetDir.y = 0;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime*lookAtSpeed);
+        
+        if(!_canFire)
+            _fireRateCounter += Time.deltaTime;
+
+        if (_fireRateCounter >= _rate)
+        {
+            _canFire = true;
+            _fireRateCounter = 0;
+        }
     }
 
     void OnTriggerStay(Collider other)
     {
         if (other.TryGetComponent<EnemyBase>(out EnemyBase enemyBase))
         {
-            _enemyPos = enemyBase.transform.position;
+            _enemyPos = enemyBase.transform.position;    
+            if (_canFire)
+            {
+                foreach (var shooter in shooters)
+                    shooter.Shoot();
+                _canFire = false;
+            }
         }
     }
-
 }
--- Top-Down-Shooter/Assets/Scripts/Drone/DroneBase.cs
+++ Top-Down-Shooter/Assets/Scripts/Drone/DroneBase.cs
@@ -1,17 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class DroneBase : MonoBehaviour
 {
-    #region movement
-    Vector3 _move;
-    [SerializeField] float _speed;
-    [SerializeField] float _dist;
-    float _range;
-    #endregion
-    
     #region Rotate around tha player
+    Vector3 _move;
     Vector3 _positionOffset;
     float _angle;
     [SerializeField] float rotationSpeed = 1;
@@ -25,18 +18,13 @@ public class DroneBase : MonoBehaviour
 
     void DroneMovementWithRotation()
     {
-        _range = Vector3.Distance(transform.position, _move);
-        if (_range >= _dist)
-            transform.position = Vector3.MoveTowards(transform.position, _move, _speed * Time.deltaTime);
-        else{
-            _positionOffset.Set(
+        _positionOffset.Set(
             Mathf.Cos( _angle ) * circleRadius,
             0,
             Mathf.Sin( _angle ) * circleRadius
             );
-            transform.position = _move + _positionOffset;
-            _angle += Time.deltaTime * rotationSpeed;
-        }        
+        transform.position = _move + _positionOffset;
+        _angle += Time.deltaTime * rotationSpeed;       
     }
 
 
