adc554796f07b7b29f530ab1173f8f18a6fc45f9
Furkan YÜKSEL <furkan1928yuksel@gmail.com>
2022-12-27 21:29:23 +0300

    Edited gamelevel

--- Top-Down-Shooter/Assets/Scripts/Managers/GameLevel.cs
+++ Top-Down-Shooter/Assets/Scripts/Managers/GameLevel.cs
@@ -7,18 +7,15 @@ public class GameLevel : Singleton<GameLevel>
     [SerializeField] int _playerLevel = 1;
     [SerializeField] List<GameObject> spawnGate = new List<GameObject>();
     [SerializeField] GameObject _parent;
-    float posX = 450;
-    float posZ = 450;
     Vector3 _playerPos;
-    float _levelSpawnTime;
-
-
 
     void SpawnGateWithLevelRate(int level)
     {
         _playerLevel = level;
-        Instantiate(spawnGate[Random.Range(0,2)], new Vector3(_playerPos.x+50, 0f, _playerPos.z+50), Quaternion.identity, _parent.transform);
-
+        for (int i = 0; i < _playerLevel; i++)
+        {
+            Instantiate(spawnGate[Random.Range(0,2)], new Vector3(_playerPos.x+(20+_playerLevel), 0f, _playerPos.z+(20+_playerLevel)), Quaternion.identity, _parent.transform);   
+        }
     }
 
     void PlayerPosition(Vector3 pos)
